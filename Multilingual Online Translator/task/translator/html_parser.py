2539cd6215d9d3ed123c6d0a9bc5355de26d0b1e
bushuev-alex <bushuev-alex@mail.ru>
2023-09-06 23:45:22 +0700

    1. argparser now parse arguments as optional arguments with default values 2. delete function that prints result from file, now print exactly with wrighting (append) in file 3. separate validate_langs on validate_langs and handle_all_query functions

--- Multilingual Online Translator/task/translator/html_parser.py
+++ Multilingual Online Translator/task/translator/html_parser.py
@@ -24,17 +24,20 @@ class Translator:
         elif self.translate_to.capitalize() not in self.languages:
             print(f"Sorry, the program doesn't support {self.translate_to}")
             return False
+        return True
 
+    def handle_all_query(self) -> bool:
         if self.translate_to in ['all', 'All']:
-            self.translate_to = self.languages.copy()
-            self.translate_to.remove('All')
-            self.translate_to.remove(self.home_lang.capitalize())
-            return True
+            translate_to = self.languages.copy()
+            translate_to.remove('All')
+            translate_to.remove(self.home_lang.capitalize())
+            self.translate_to = translate_to
+        else:  # if len(self.translate_to == 1)
+            self.translate_to = [self.translate_to]
         return True
 
     def send_request(self, translate_to: str, word: str) -> BeautifulSoup:
         url = self.url + f"{self.home_lang}-{translate_to.lower()}/{word}"
-        print(translate_to)
         request_result = requests.get(url, headers=self.headers)
         if request_result.status_code == 200:
             return BeautifulSoup(request_result.content, 'html.parser')
@@ -53,32 +56,24 @@ class Translator:
     def fetch_sentences(self, soup: BeautifulSoup) -> list[tuple[str, str]]:
         untranslated_sents = soup.findAll('div', {'class': 'src ltr'})
         translated_sents = soup.findAll('div', {'class': re.compile("trg.*(ltr|rtl).*")})
-        sentences = [(untranslated.text.strip(),
-                     translated.text.strip()) if untranslated and translated else ('', '')
+        sentences = [(untranslated.text.strip(), translated.text.strip())
+                     if untranslated and translated else ('', '')
                      for untranslated, translated in zip(untranslated_sents, translated_sents)]
-        # sentences = ((untranslated.text.strip(),
-        #              translated.text.strip())
-        #              for untranslated, translated in zip(untranslated_sents, translated_sents)
-        #              if untranslated and translated)
         return sentences
 
-    def write_words_sents(self, language: str, words: list, sentences: list):
+    def write_words_sents(self, language: str, words: list, sentences: list) -> bool:
         with open(f"{self.word}.txt", 'a', encoding='UTF-8') as file:
-            file.write(f"{language} Translations:\n")
-            file.write(f"{words[0]}\n\n")
-            file.write(f"{language} Examples:\n")
-            file.write(f"{sentences[0][0]}\n")
-            file.write(f"{sentences[0][1]}\n\n")
-
-    def print_result(self):
-        try:
-            with open(f"{self.word}.txt", 'r', encoding='UTF-8') as file:
-                for line in file:
-                    print(line.strip())
-        except FileNotFoundError:
-            print("File doesn't exist")
-        finally:
-            print("OK")
+            print(f"{language} Translations:", file=file)
+            print(f"{language} Translations:")
+            print(f"{words[0]}\n", file=file)
+            print(f"{words[0]}\n")
+            print(f"{language} Examples:", file=file)
+            print(f"{language} Examples:")
+            print(f"{sentences[0][0]}", file=file)
+            print(f"{sentences[0][0]}")
+            print(f"{sentences[0][1]}\n", file=file)
+            print(f"{sentences[0][1]}\n")
+        return True
 
 
 if __name__ == '__main__':
--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -4,9 +4,9 @@ import argparse
 
 def parse_params() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("home_lang", default="russian")
-    parser.add_argument("translate_to", default="all")
-    parser.add_argument("word", default="Папа")
+    parser.add_argument("--home_lang", default="russian")
+    parser.add_argument("--translate_to", default="all")
+    parser.add_argument("--word", default="Папа")
     args = parser.parse_args()
     return args
 
@@ -16,12 +16,13 @@ def main():
     my_translator = Translator(args.home_lang, args.translate_to, args.word)
 
     if my_translator.validate_langs():
+        my_translator.handle_all_query()
         for lang in my_translator.translate_to:
             soup = my_translator.send_request(lang, my_translator.word)
             words = my_translator.fetch_words(soup)
             sentences = my_translator.fetch_sentences(soup)
             my_translator.write_words_sents(lang, words, sentences)
-        my_translator.print_result()
+        print("END")
 
 
 if __name__ == '__main__':
